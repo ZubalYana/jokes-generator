dd58c06504d5e5d896bfe00ccf23aeb90aed9e50
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-06-21 15:32:42 +0300

    connect mongoDB

--- server/app.js
+++ server/app.js
@@ -2,5 +2,11 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
 const PORT = 5000;
+const mongoose = require('mongoose');
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.log(err));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
