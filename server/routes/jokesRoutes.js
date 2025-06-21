f130e4560eaa36e2eb442faa69acb1b523fbfb0d
Martakomarnicka20 <146972743+Martakomarnicka20@users.noreply.github.com>
2025-06-21 16:03:43 +0300

    get joke random

--- server/routes/jokesRoutes.js
+++ server/routes/jokesRoutes.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const Joke = require('../models/Joke');
+router.get("/joke/random", async (req, res) => {
+  try {
+    const count = await Joke.countDocuments();
+    const randomIndex = Math.floor(Math.random() * count);
+    const joke = await Joke.findOne().skip(randomIndex);
 
-// router.post('/', (req, res) => {
-// });
+    if (!joke) {
+      return res.status(404).json({ error: "Жарт не знайдено" });
+    }
+
+    res.json({ joke: joke.text });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Помилка сервера" });
+  }
+});
 
 module.exports = router;
+
