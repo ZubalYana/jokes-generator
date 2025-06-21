398f37357aa224ba6908643b8e360cdd19be6f43
MarkivNazar <nazarmn2008@gmail.com>
2025-06-21 15:54:07 +0300

    fixe

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import './App.css'
+import SendJokes from './components/SendJokes/SendJokes'
+import './index.css'
 
 function App() {
 
   return (
     <>
+       <h1 className="text-3xl font-bold underline bg-red-500">
+      Hello world!
+    </h1>
     </>
   )
 }
--- src/components/SendJokes/SendJokes.tsx
+++ src/components/SendJokes/SendJokes.tsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function SendJokes() {
   return (
-    <div>SendJokes</div>
+    <div className='w-full h-[100vh] flex flex-col'>
+
+      <h2 className='text-center text-3xl'>Send Jokes</h2>
+
+      <input type="text" placeholder='Емейл' className='w-[80%] h-[40px] pl-2 '/>
+
+      <textarea name="" id="" placeholder='Напишите свою шутку'/>
+
+      <button>Відправити на веріфікацію</button>
+
+
+    </div>
   )
 }
